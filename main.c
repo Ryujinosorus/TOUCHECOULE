100897165529eaeecc8f5a6391512b74fa9b22ba
jasongst <jasonguestin@gmail.com>
2020-12-13 18:22:31 +0100

    Ajout de nos noms dans le main.c

--- main.c
+++ main.c
@@ -4,9 +4,15 @@
 #include "Game.h"
 #include "Plateau.h"
 
+/* 
+Jeu de la bataille navale - Projet de CAV 2020, réalisé par:
 
+- ES-SEBBANI Naïm
+- GUESTIN Jason
+*/
 
 int main(){
     srand( time( NULL ) );
     run(initGame());
 }
+¨
